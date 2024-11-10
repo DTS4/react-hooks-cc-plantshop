20c31a26f0810b9cebbc3dc08b59f2cb4915ff40
Keith <keith.githinji@student.moringaschool.com>
2024-11-10 17:31:50 +0300

    Completed the Code Challenge

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, {useState, usestate} from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+  const [inStock, setInStock] = useState();
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      <button className={inStock ? "primary" : ""} onClick={() => setInStock(!inStock)}>
+        {inStock ? "In Stock" : "Out of Stock"}
+      </button>
     </li>
   );
 }
